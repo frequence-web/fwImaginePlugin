a8b54d97a3610f97e74fe4141c8d52fdd3db0a9f
yohang <yohan@giarelli.org>
2011-08-29 16:07:47 +0200

    Refactored controller

--- modules/fwImagine/actions/actions.class.php
+++ modules/fwImagine/actions/actions.class.php
@@ -4,48 +4,48 @@ class fwImagineActions extends sfActions
 {
   public function executeFilter(sfWebRequest $request)
   {
+    // Get the filters
     $filters = $request->getParameter('filters');
+
+    // Resolve paths
     $path = sfConfig::get('sf_web_dir').'/'.$request->getParameter('path');
     $destPath = sfConfig::get('sf_web_dir').'/media/cache/'.$filters.'/'.$request->getParameter('path');
 
-    try
+    // Set image header
+    // TODO : make it configurable
+    $this->getResponse()->setContentType('image/png');
+    $this->getResponse()->addCacheControlHttpHeader('max_age=31536000');
+
+    // If the file doesn't exists
+    if (!is_file($destPath))
     {
-      $this->getResponse()->setContentType('image/png');
-      $this->getResponse()->addCacheControlHttpHeader('max_age=31536000');
-      
-      if (!is_file($destPath))
+      // Create directory structure
+      $destDir = dirname($destPath);
+      if (!is_dir($destDir))
       {
-        $destDir = dirname($destPath);
-        if (!is_dir($destDir))
-        {
-          mkdir($destDir, 0777, true);
-        }
-
-        $image = $this->getContext()->getImagine()->open($path);
-
-        foreach (explode(',', $filters) as $filter)
-        {
-          $image = $this->getContext()->getImagineFilterManager()->get($filter)
-                                      ->apply($image);
-        }
-
-        ob_start();
-
-        $image->show('png')
-              ->save($destPath, array('quality' => 100, 'format' => 'png'));
-
-        $this->getResponse()->setContent(ob_get_clean());
-
-        return sfView::NONE;
+        mkdir($destDir, 0777, true);
       }
 
-      $this->getResponse()->setContent(file_get_contents($destPath));
+      // Open source and apply filters
+      $image = $this->getContext()->getImagine()->open($path);
+      foreach (explode(',', $filters) as $filter)
+      {
+        $image = $this->getContext()->getImagineFilterManager()->get($filter)
+                                    ->apply($image);
+      }
 
-      return sfView::NONE;
+      // Save (cache)
+      $image->save($destPath, array('quality' => 100, 'format' => 'png'));
+
+      // Send it
+      $this->getResponse()->setContent($image->get('png'));
     }
-    catch (Exception $e)
+    else
     {
-      throw $e;
+      // Just send it
+      $this->getResponse()->setContent(file_get_contents($destPath));
     }
+
+    return sfView::NONE;
   }
 }
